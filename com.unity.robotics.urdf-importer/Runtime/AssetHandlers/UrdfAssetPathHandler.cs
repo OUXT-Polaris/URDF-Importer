c0f5e65849f9515f412c7f4b3f3a885ff216bfe0
Masaya Kataoka <ms.kataoka@gmail.com>
2021-09-21 00:12:45 +0900

    remove wrong section, any type or URI can be allowed in filename
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- com.unity.robotics.urdf-importer/Runtime/AssetHandlers/UrdfAssetPathHandler.cs
+++ com.unity.robotics.urdf-importer/Runtime/AssetHandlers/UrdfAssetPathHandler.cs
@@ -82,19 +82,6 @@ namespace Unity.Robotics.UrdfImporter
 
         public static string GetRelativeAssetPathFromUrdfPath(string urdfPath, bool convertToPrefab=true)
         {
-            if (!urdfPath.StartsWith(@"package://"))
-            {
-               Debug.LogWarning(@$"{urdfPath} is not a valid URDF package file path. Path should start with package://, and URDF file should be in the directory root.");
-               if (urdfPath.Substring(0, 3) == "../")
-               {
-                   Debug.LogWarning("Attempting to replace file path's starting instance of `../` with standard package notation `package://` to prevent manual path traversal at root of directory!");
-                   urdfPath = $@"package://{urdfPath.Substring(3)}";
-               }
-               else
-               {
-                   return null;
-               }
-            }
             string path;
             if (urdfPath.StartsWith(@"package://"))
             {
